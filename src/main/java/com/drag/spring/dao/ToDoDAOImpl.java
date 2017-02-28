cf391b6ad56ba3c333582be81cd61014b1af658a
Drag <drag.faq@gmail.com>
2017-02-28 18:17:30 +0200

    In Progress

--- src/main/java/com/drag/spring/dao/ToDoDAOImpl.java
+++ src/main/java/com/drag/spring/dao/ToDoDAOImpl.java
@@ -1,6 +1,7 @@
 package com.drag.spring.dao;
 
 import com.drag.spring.model.ToDo;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
@@ -20,38 +21,43 @@ public class ToDoDAOImpl implements ToDoDAO {
         this.sessionFactory = sessionFactory;
     }
 
+    @Override
     public void addToDo(ToDo toDo) {
         Session session = this.sessionFactory.getCurrentSession();
         session.persist(toDo);
         logger.info("TODO saved. [" + toDo + "]");
     }
 
+    @Override
     public void updateToDo(ToDo toDo) {
         Session session = this.sessionFactory.getCurrentSession();
         session.update(toDo);
         logger.info("TODO updated. [" + toDo + "]");
     }
 
+    @Override
     public void removeToDo(int id) {
         Session session = this.sessionFactory.getCurrentSession();
-        ToDo toDo = session.load(ToDo.class, new Integer(id));
+        ToDo toDo = (ToDo) session.load(ToDo.class, new Integer(id));
         if (toDo != null)
             session.delete(toDo);
         logger.info("TODO deleted. [" + toDo + "]");
     }
 
+    @Override
     public ToDo getToDoById(int id) {
         Session session = this.sessionFactory.getCurrentSession();
-        ToDo toDo = session.load(ToDo.class, new Integer(id));
+        ToDo toDo = (ToDo) session.load(ToDo.class, new Integer(id));
         logger.info("TODO loaded. [" + toDo + "]");
 
         return toDo;
     }
 
     @SuppressWarnings("unchecked")
+    @Override
     public List<ToDo> listToDosByStatus(int status) {
         Session session = this.sessionFactory.getCurrentSession();
-        String filter = "from todolist";
+        String filter = "from ToDo";
         if (status == 0 || status == 1)
             filter += " WHERE isDone = " + status;
         List<ToDo> list = session.createQuery(filter).list();
--- src/main/java/com/drag/spring/service/ToDoServiceImpl.java
+++ src/main/java/com/drag/spring/service/ToDoServiceImpl.java
@@ -16,26 +16,31 @@ public class ToDoServiceImpl implements ToDoService {
         this.toDoDAO = toDoDAO;
     }
 
+    @Override
     @Transactional
     public void addToDo(ToDo toDo) {
         this.toDoDAO.addToDo(toDo);
     }
 
+    @Override
     @Transactional
     public void updateToDo(ToDo toDo) {
         this.toDoDAO.updateToDo(toDo);
     }
 
+    @Override
     @Transactional
     public void removeToDo(int id) {
         this.toDoDAO.removeToDo(id);
     }
 
+    @Override
     @Transactional
     public ToDo getToDoById(int id) {
         return this.toDoDAO.getToDoById(id);
     }
 
+    @Override
     @Transactional
     public List<ToDo> listToDosByStatus(int status) {
         return this.toDoDAO.listToDosByStatus(status);
